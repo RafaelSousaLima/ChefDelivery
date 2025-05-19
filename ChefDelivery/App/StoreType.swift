04c24095a08b2d9b6f9a76b30963a55e616f9633
Rafael Lima <rafaelsousalima@live.com>
2025-05-19 16:35:18 -0300

    Alterações do proejto de gerenciamento de estado e navegação

--- ChefDelivery/App/StoreType.swift
+++
@@ -1,8 +0,0 @@
-//
-//  StoreType.swift
-//  ChefDelivery
-//
-//  Created by Rafael Lima on 15/05/25.
-//
-
-import Foundation
--- ChefDelivery/StoresView/StoresContainerView.swift
+++ ChefDelivery/StoresView/StoresContainerView.swift
@@ -11,14 +11,84 @@ struct StoresContainerView: View {
     
     let title = "Lojas"
     
+    @State private var ratingFilter: Int = 0
+    
+    @State private var distance: Double = 50;
+    
+    var filteredStores: [StoreType] {
+        return storesMock.filter { store in
+            store.stars >= ratingFilter && store.distance <= distance
+        }
+    }
+    
     var body: some View {
         VStack(alignment: .leading) {
-            Text(title).font(.headline)
-            VStack(alignment: .leading, spacing: 30) {
-                ForEach(storesMock) { mock in
-                    StoreItemView(order: mock)
-                }
+            HStack {
+                Text(title).font(.headline)
+                
+                Spacer()
+                
+                Menu("Estrelas") {
+                    
+                    Button {
+                        ratingFilter = 0
+                    } label: {
+                        Text("Limpar filtro")
+                        Divider()
+                    }
+                    
+                    ForEach(1...5, id: \.self) { rating in
+                        Button {
+                            ratingFilter = rating
+                        } label: {
+                            if rating > 1 {
+                                Text("\(rating) estrelas ou mais")
+                            } else {
+                                Text("\(rating) estrelas")
+                            }
+                        }
+                    }
+                }.foregroundColor(.black)
+                
+                Menu("Distancia") {
+                    
+                    Button {
+                        distance = 0
+                    } label: {
+                        Text("Limpar filtro")
+                        Divider()
+                    }
+                    
+                    ForEach(Array(stride(from: 5, to: 30, by: 5)), id: \.self) { distance in
+                        Button {
+                            self.distance = distance
+                        } label: {
+                            Text("Até \(distance.formatted()) km")
+                        }
+                    }
+                }.foregroundColor(.black)
+                
             }
+            VStack(alignment: .leading, spacing: 30) {
+                
+                if filteredStores.isEmpty {
+                    Text("Nenhuma loja encontrada.")
+                        .font(.title2)
+                        .bold()
+                        .foregroundColor(Color("ColorRed"))
+                        .padding(.vertical, 20)
+                        .frame(maxWidth: .infinity)
+                } else {
+                    ForEach(filteredStores) { mock in
+                        NavigationLink {
+                            StoreDetailView(store: mock)
+                        } label: {
+                            StoreItemView(store: mock)
+                        }
+                    }
+                }
+                
+            }.foregroundColor(.black)
         }.padding(20)
     }
 }
