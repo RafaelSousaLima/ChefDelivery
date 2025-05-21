159f5c0a593c9736f9af5d6360cf42396de7ed7a
Rafael Lima <rafaelsousalima@live.com>
2025-05-21 11:41:30 -0300

    Requisição http

--- ChefDelivery/App/ContentView.swift
+++ ChefDelivery/App/ContentView.swift
@@ -8,22 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //MARK: -Atributos
+    let homeService = HomeService()
+    @State private var stores: [StoreType] = []
+    
+    @State private var isLoading = true
+    
+    //MARK: - View
     var body: some View {
         NavigationView {
             VStack {
-                NavigationBar()
-                    .padding(.horizontal, 15)
-                ScrollView(.vertical, showsIndicators: false) {
-                    VStack(spacing: 20) {
-                        OrderTypeGridView()
-                        CarouselTabView()
-                        StoresContainerView()
+                if isLoading {
+                    ProgressView()
+                        .padding()
+                } else {
+                    NavigationBar()
+                        .padding(.horizontal, 15)
+                    ScrollView(.vertical, showsIndicators: false) {
+                        VStack(spacing: 20) {
+                            OrderTypeGridView()
+                            CarouselTabView()
+                            StoresContainerView(stores: stores)
+                        }
                     }
                 }
             }
             .padding()
         }
+        .onAppear {
+            Task {
+                await self.getStores()
+            }
+            getStoresWithAlamoFire()
+        }
     }
+    
+    //MARK: Methods
+    func getStores() async {
+        do {
+            let result = try await homeService.fetchDataNative()
+            switch result {
+                case .success(let stores):
+                    self.stores = stores
+                    self.isLoading = false
+                case .failure(let error):
+                self.isLoading = false
+                print(error.localizedDescription)
+            }
+        } catch {
+            self.isLoading = false
+            print(error.localizedDescription)
+        }
+        
+    }
+    
+    func getStoresWithAlamoFire() {
+        homeService.fetchDataAlamo { stores, error in
+            print(stores as Any)
+        }
+    }
+    
 }
 
 #Preview {
